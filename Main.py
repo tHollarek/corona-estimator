ac0e81650b69de4291ac9b062ecf05b6da493b07
Tobias Hollarek <t.hollarek@gmail.com>
2020-03-19 21:19:50 +0100

    delete obsolte comments

--- Main.py
+++ Main.py
@@ -4,8 +4,5 @@ from Forecasting import show_forecast, show_forecast_with_lockdown
 days_to_forecast = 50
 lockdown_date = date(2020, 3, 16)
 
-# calculate_forecast(days_to_forecast, southkorea)
-# calculate_forecast(days_to_forecast, germany)
-# calculate_forecast(days_to_forecast, italy)
 show_forecast(days_to_forecast, Country.SPAIN)
-show_forecast_with_lockdown(days_to_forecast, lockdown_date, Country.SPAIN)
+# show_forecast_with_lockdown(days_to_forecast, lockdown_date, Country.SPAIN)
